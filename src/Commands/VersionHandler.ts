661c51f97fe2f0fc69677b0f8a26f75267463c2d
Porta <portalmaster137@gmail.com>
2023-11-27 02:38:25 +1000

    yay

--- src/Commands/VersionHandler.ts
+++ src/Commands/VersionHandler.ts
@@ -6,17 +6,18 @@ logger.level = 'debug';
 import * as ChildProcess from "child_process";
 
 const MAJOR_VERSION = 1;
-const MINOR_VERSION = 1;
-const PATCH_VERSION = 3;
+const MINOR_VERSION = 2;
+const PATCH_VERSION = 0;
 
 class VersionHandler {
     static async handle(interaction: Discord.CommandInteraction, prismaClient: PrismaClient) {
 
         const ghash = ChildProcess.execSync('git rev-parse HEAD').toString().trim();
+        const ccount = ChildProcess.execSync('git rev-list --count HEAD').toString().trim();
 
         await interaction.reply({
             ephemeral: true,
-            content: `Version ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}\nGit hash: ${ghash}`
+            content: `Version ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}\nGit hash: ${ghash}\nCommit Count: ${ccount}`
         });
     }
 }
